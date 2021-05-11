cbdb019d64de91b2aadf86cf23aab8d23e3f226d
Germán Castaño Roldán <gercasta94@gmail.com>
2021-05-11 12:36:52 +0200

    Add cuda/lud with time measurement

---
+++ rodinia/cuda/lud/cuda/lud.cu
@@ -0,0 +1,234 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lud.cu
+ *
+ *    Description:  The main wrapper for the suite
+ *
+ *        Version:  1.0
+ *        Created:  10/22/2009 08:40:34 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Liang Wang (lw2aw), lw2aw@virginia.edu
+ *        Company:  CS@UVa
+ *
+ * =====================================================================================
+ */
+
+#include <cuda.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "common.h"
+
+#ifdef TIME_IT
+#include <sys/time.h>
+#endif
+
+#ifdef RD_WG_SIZE_0_0
+        #define BLOCK_SIZE RD_WG_SIZE_0_0
+#elif defined(RD_WG_SIZE_0)
+        #define BLOCK_SIZE RD_WG_SIZE_0
+#elif defined(RD_WG_SIZE)
+        #define BLOCK_SIZE RD_WG_SIZE
+#else
+        #define BLOCK_SIZE 16
+#endif
+
+static int do_verify = 0;
+
+static struct option long_options[] = {
+  /* name, has_arg, flag, val */
+  {"input", 1, NULL, 'i'},
+  {"size", 1, NULL, 's'},
+  {"verify", 0, NULL, 'v'},
+  {0,0,0,0}
+};
+
+#ifdef TIME_IT
+extern long long
+#else
+extern void
+#endif
+lud_cuda(float *d_m, int matrix_dim);
+
+#ifdef TIME_IT
+long long get_time() {
+        struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000000) + tv.tv_usec;
+}
+#endif
+
+int
+main ( int argc, char *argv[] )
+{
+  #ifdef TIME_IT
+  long long initTime;
+  long long alocTime = 0;
+  long long cpinTime = 0;
+  long long kernTime = 0;
+  long long cpouTime = 0;
+  long long freeTime = 0;
+  long long aux1Time;
+  long long aux2Time;
+  #endif
+
+  #ifdef TIME_IT
+  aux1Time = get_time();
+  cudaThreadSynchronize();
+  aux2Time = get_time();
+  initTime = aux2Time-aux1Time;
+  #endif
+
+  printf("WG size of kernel = %d X %d\n", BLOCK_SIZE, BLOCK_SIZE);
+
+  int matrix_dim = 32; /* default matrix_dim */
+  int opt, option_index=0;
+  func_ret_t ret;
+  const char *input_file = NULL;
+  float *m, *d_m, *mm;
+  stopwatch sw;
+
+  while ((opt = getopt_long(argc, argv, "::vs:i:", 
+                            long_options, &option_index)) != -1 ) {
+    switch(opt){
+    case 'i':
+      input_file = optarg;
+      break;
+    case 'v':
+      do_verify = 1;
+      break;
+    case 's':
+      matrix_dim = atoi(optarg);
+      printf("Generate input matrix internally, size =%d\n", matrix_dim);
+      // fprintf(stderr, "Currently not supported, use -i instead\n");
+      // fprintf(stderr, "Usage: %s [-v] [-s matrix_size|-i input_file]\n", argv[0]);
+      // exit(EXIT_FAILURE);
+      break;
+    case '?':
+      fprintf(stderr, "invalid option\n");
+      break;
+    case ':':
+      fprintf(stderr, "missing argument\n");
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-v] [-s matrix_size|-i input_file]\n",
+	      argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+  
+  if ( (optind < argc) || (optind == 1)) {
+    fprintf(stderr, "Usage: %s [-v] [-s matrix_size|-i input_file]\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (input_file) {
+    printf("Reading matrix from file %s\n", input_file);
+    ret = create_matrix_from_file(&m, input_file, &matrix_dim);
+    if (ret != RET_SUCCESS) {
+      m = NULL;
+      fprintf(stderr, "error create matrix from file %s\n", input_file);
+      exit(EXIT_FAILURE);
+    }
+  } 
+  else if (matrix_dim) {
+    printf("Creating matrix internally size=%d\n", matrix_dim);
+    ret = create_matrix(&m, matrix_dim);
+    if (ret != RET_SUCCESS) {
+      m = NULL;
+      fprintf(stderr, "error create matrix internally size=%d\n", matrix_dim);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+
+  else {
+    printf("No input file specified!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (do_verify){
+    printf("Before LUD\n");
+    // print_matrix(m, matrix_dim);
+    matrix_duplicate(m, &mm, matrix_dim);
+  }
+
+  #ifdef TIME_IT
+  aux1Time = get_time();
+  #endif
+  cudaMalloc((void**)&d_m, 
+             matrix_dim*matrix_dim*sizeof(float));
+  #ifdef TIME_IT
+  aux2Time = get_time();
+  alocTime += aux2Time-aux1Time;
+  #endif
+
+  /* beginning of timing point */
+  stopwatch_start(&sw);
+  #ifdef TIME_IT
+  aux1Time = get_time();
+  #endif
+  cudaMemcpy(d_m, m, matrix_dim*matrix_dim*sizeof(float), 
+	     cudaMemcpyHostToDevice);
+  #ifdef TIME_IT
+  aux2Time = get_time();
+  cpinTime += aux2Time-aux1Time;
+  kernTime = 
+  #endif
+  lud_cuda(d_m, matrix_dim);
+  #ifdef TIME_IT
+  aux1Time = get_time();
+  #endif
+  cudaMemcpy(m, d_m, matrix_dim*matrix_dim*sizeof(float), 
+	     cudaMemcpyDeviceToHost);
+  #ifdef TIME_IT
+  aux2Time = get_time();
+  cpouTime += aux2Time-aux1Time;
+  #endif
+  /* end of timing point */
+  stopwatch_stop(&sw);
+  printf("Time consumed(ms): %lf\n", 1000*get_interval_by_sec(&sw));
+  #ifdef TIME_IT
+  aux1Time = get_time();
+  #endif
+  cudaFree(d_m);
+  #ifdef TIME_IT
+  aux2Time = get_time();
+  freeTime += aux2Time-aux1Time;
+  #endif
+
+  if (do_verify){
+    printf("After LUD\n");
+    // print_matrix(m, matrix_dim);
+    printf(">>>Verify<<<<\n");
+    lud_verify(mm, m, matrix_dim); 
+    free(mm);
+  }
+
+  free(m);
+
+  #ifdef TIME_IT
+  long long totalTime = initTime + alocTime + cpinTime + kernTime + cpouTime + freeTime;
+	printf("Time spent in different stages of GPU_CUDA KERNEL:\n");
+
+	printf("%15.12f s, %15.12f % : GPU: SET DEVICE / DRIVER INIT\n",	(float) initTime / 1000000, (float) initTime / (float) totalTime * 100);
+	printf("%15.12f s, %15.12f % : GPU MEM: ALO\n", 					(float) alocTime / 1000000, (float) alocTime / (float) totalTime * 100);
+	printf("%15.12f s, %15.12f % : GPU MEM: COPY IN\n",					(float) cpinTime / 1000000, (float) cpinTime / (float) totalTime * 100);
+
+	printf("%15.12f s, %15.12f % : GPU: KERNEL\n",						(float) kernTime / 1000000, (float) kernTime / (float) totalTime * 100);
+
+	printf("%15.12f s, %15.12f % : GPU MEM: COPY OUT\n",				(float) cpouTime / 1000000, (float) cpouTime / (float) totalTime * 100);
+	printf("%15.12f s, %15.12f % : GPU MEM: FRE\n", 					(float) freeTime / 1000000, (float) freeTime / (float) totalTime * 100);
+
+	printf("Total time:\n");
+	printf("%.12f s\n", 												(float) totalTime / 1000000);
+	#endif
+
+  return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
